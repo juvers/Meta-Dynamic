15ea87dbc69a2d3088b63a3151170a6e9478c190
Jude Kuti <judekuti@gmail.com>
2022-11-21 14:34:06 -0600

    Update meta data sample

--- server/poc/meta.js
+++ server/poc/meta.js
@@ -1,18 +1,18 @@
 const metas = [
   {
-    title: "Post #1",
+    title: "Hour Glass Meta title",
     description: "This is a hour glass",
     image:
       "https://media.alle.com/image/upload/v1597159240/alle/icons/HOUR_GLASS_NUDE.png",
   },
   {
-    title: "Post #2",
+    title: "Gift Meta title",
     description: "This is a gift",
     image:
       "https://media.alle.com/image/upload/v1597159006/alle/icons/GIFT_NUDE.png",
   },
   {
-    title: "Post #3",
+    title: "Coin Meta title",
     description: "This is a coin",
     image:
       "https://media.alle.com/image/upload/v1597159146/alle/icons/COIN_NUDE.png",
